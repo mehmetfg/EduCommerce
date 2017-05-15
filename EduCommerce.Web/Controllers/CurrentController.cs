57c326e944f663157fd12c6b2c0c2c61806e4f02
Mehmet Fatih GÖÇGÜN <mehmetfg@gmail.com>
2017-05-15 14:36:44 +0300

    yeni

--- EduCommerce.Web/Controllers/CurrentController.cs
+++ EduCommerce.Web/Controllers/CurrentController.cs
@@ -15,7 +15,7 @@ namespace EduCommerce.Web.Controllers
         // GET: Current
         public ActionResult Index()
         {
-            return View();
+            return RedirectToAction("ListCurrent");
         }
         public ActionResult ListCurrent()
         { 
@@ -31,12 +31,81 @@ namespace EduCommerce.Web.Controllers
 
             return View(model);
         }
+
+        public ActionResult Detay(int id)
+        {
+            Current edu = (from p in islemler.Current
+                              where p.ID == id
+                              select p).FirstOrDefault();
+            ViewBag.ID = 1;
+            return View(edu);
+        }
         public ActionResult Yeni()
         {
 
 
+
+            return View();
         }
-      
-     
+        [HttpPost]
+        public ActionResult Yeni(Current cari)
+        {
+            islemler.Current.Add(cari);
+            islemler.SaveChanges();
+
+            RedirectToAction("ListCurrent");
+            return View();
+
+        }
+
+        public ActionResult Guncelle(int id)
+        {
+            ViewBag.Id = 1;
+            Current cari = (from p in islemler.Current
+                                    where p.ID == id
+                                    select p).FirstOrDefault();
+            return View(cari);
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Current cari)
+        {
+            Current seciliCari = (from p in islemler.Current
+                                    where p.ID == cari.ID
+                                    select p).FirstOrDefault();
+            seciliCari.DealerID = cari.DealerID;
+            seciliCari.Class = cari.Class;
+            seciliCari.Quantity = cari.Quantity;
+            seciliCari.Status = cari.Status;
+            seciliCari.Type = cari.Type;
+            seciliCari.StudentID = cari.StudentID;
+            islemler.SaveChanges();
+            return View();
+
+
+        }
+        public ActionResult Sil(int id)
+        {
+            Current cari = (from p in islemler.Current
+                                    where p.ID == id
+                                    select p).FirstOrDefault();
+            return View(cari);
+        }
+        [HttpPost, ActionName("Sil")]
+        public ActionResult SilmeIslemi(int id)
+        {
+            Current silinecekCari = (from p in islemler.Current
+                                          where p.ID == id
+                                          select p).FirstOrDefault();
+
+            int egitimId = silinecekCari.ID;
+            islemler.Current.Remove(silinecekCari);
+            islemler.SaveChanges();
+            return RedirectToAction("ListCurrent");
+
+
+
+        }
+
+
     }
 }
