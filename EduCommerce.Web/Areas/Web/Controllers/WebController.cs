cc517832eb0775215135396a5a201bb6669aeb6a
Mehmet Fatih GÖÇGÜN <mehmetfg@gmail.com>
2017-06-02 16:39:03 +0300

    Revert "Revert "new""
    This reverts commit 22d6cc666070c63423ebfdcbd1bf89a143c15726.

---
+++ EduCommerce.Web/Areas/Web/Controllers/WebController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EduCommerce.Web.Areas.Web.Controllers
+{
+    public class WebController : Controller
+    {
+        // GET: Web/Web
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
